72237ca6884df10bcc8170c067809a003a1b880c
Lucas <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-08 22:01:18 -0300

    Create AlunoRepositorio.java

---
+++ src/main/java/br/edu/ifpr/springacessobanco/AlunoRepositorio.java
@@ -0,0 +1,9 @@
+package br.edu.ifpr.springacessobanco;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepositorio extends CrudRepository<Aluno, Integer> {
+    
+}
