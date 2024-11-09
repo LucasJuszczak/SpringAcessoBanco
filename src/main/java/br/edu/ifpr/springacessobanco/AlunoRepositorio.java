088f0913f021399646a183ee326615ebf1da512c
Lucas <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-08 22:03:02 -0300

    Update AlunoRepositorio.java

--- src/main/java/br/edu/ifpr/springacessobanco/AlunoRepositorio.java
+++ src/main/java/br/edu/ifpr/springacessobanco/AlunoRepositorio.java
@@ -4,6 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlunoRepositorio extends CrudRepository<Aluno, Integer> {
-    
-}
+public interface AlunoRepositorio extends CrudRepository<Aluno, Integer> { }
