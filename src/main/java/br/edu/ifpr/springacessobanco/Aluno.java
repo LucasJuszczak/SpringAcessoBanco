975e2a85d9716322e926f06499d811d7bb58baef
Lucas <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-08 21:56:02 -0300

    Update Aluno.java

--- src/main/java/br/edu/ifpr/springacessobanco/Aluno.java
+++ src/main/java/br/edu/ifpr/springacessobanco/Aluno.java
@@ -1,4 +1,23 @@
 package br.edu.ifpr.springacessobanco;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+
+    private String endereco;
+
 }
